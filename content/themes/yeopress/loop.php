28cc6f1ed77eaae8e37c2af183bcfe53b7fc902c
Kadosa Orosz <orosz.kadosa@gmail.com>
2014-07-06 17:50:49 +0200

    Second commit, setting up yeopress and stuff.

--- content/themes/yeopress/loop.php
+++ content/themes/yeopress/loop.php
@@ -1,6 +1,7 @@
 <?php if (is_page()): the_post() ?>
 	<article id="page-<?php the_ID() ?>">
 		<?php the_content(); ?>
+
 	</article>
 <?php else: ?>
 	<?php if (have_posts()):
@@ -15,7 +16,7 @@
 					<h1 class="article-title"><?php if(!is_singular()): ?><a href="<?php the_permalink() ?>" title="<?php the_title_attribute() ?>"><?php endif; the_title() ?><?php if(!is_singular()): ?></a><?php endif; ?></h1>
 					<div class="article-info">
 						<span class="date"><?php the_date('m-d-Y') ?></span>
-						<span class="comments"><?php comments_popup_link(__('Leave a comment'), __('1 Comment'), __('% Comments')) ?></span> 
+						<span class="comments"><?php comments_popup_link(__('Leave a comment'), __('1 Comment'), __('% Comments')) ?></span>
 					</div>
 				</header>
 				<div class="article-content">
